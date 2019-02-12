50910420e53b0bbbe2a94da4f219bf140b7f5b73
samyin1 <45219211+samyin1@users.noreply.github.com>
2019-02-12 19:30:06 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+sudo wget https://pinlandata.oss-cn-hangzhou.aliyuncs.com/pinlandata/pinlan-pinshi-arm.tar.gz
+sudo docker load -i pinlan-pinshi-arm.tar.gz
+sudo nvidia-docker run -d -p 80:80 --env CUDA_VISIBLE_DEVICES=0 pinlan/pinshi:arm

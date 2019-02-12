930ac0811a56bad8f16563646158e9a7f0ade0ce
samyin1 <45219211+samyin1@users.noreply.github.com>
2019-02-12 16:39:26 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
 
-sudo wget https://pinlandata.oss-cn-hangzhou.aliyuncs.com/pinlandata/pinlan-pinshi-arm.tar.gz
-sudo docker load -i pinlan-pinshi-arm.tar.gz 
-sudo docker run -d --name pinshi -p 80:80 pinlan/pinshi:arm
